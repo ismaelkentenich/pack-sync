b52098b596152636a2e41d31315ee2d161f474fa
Ismael Kentenich <ismaelkentenichm@gmail.com>
2026-08-22 09:39:32 -0300

    chore: add testing scripts and dependencies for Jest

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  preset: "jest-expo",
+
+  clearMocks: true,
+
+  moduleNameMapper: {
+    "^@app/(.*)$": "<rootDir>/src/app/$1",
+    "^@assets/(.*)$": "<rootDir>/src/assets/$1",
+    "^@components/(.*)$": "<rootDir>/src/components/$1",
+    "^@features/(.*)$": "<rootDir>/src/features/$1",
+    "^@hooks/(.*)$": "<rootDir>/src/hooks/$1",
+    "^@utils/(.*)$": "<rootDir>/src/utils/$1",
+    "^@theme/(.*)$": "<rootDir>/src/theme/$1",
+    "^@store/(.*)$": "<rootDir>/src/store/$1",
+    "^@infrastructure/(.*)$":
+      "<rootDir>/src/infrastructure/$1",
+    "^@i18n/(.*)$": "<rootDir>/src/i18n/$1",
+    "^@types/(.*)$": "<rootDir>/src/types/$1",
+  },
+
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/**/index.ts",
+    "!src/i18n/locales/**",
+  ],
+
+  coverageDirectory: "coverage",
+};
